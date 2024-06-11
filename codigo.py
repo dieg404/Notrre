a20cb1b46132b386967e9b128148ea31dead9167
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-06-10 22:23:48 -0500

    v1

--- codigo.py
+++ codigo.py
@@ -1,8 +1,12 @@
+#Notas
+#V1: Se establecen la funcion que pregunta cuantas notas tiene una materia y se inicia el codigo donde se pregunta las materias que vera, el nombre y se guarda en una lista.
+
+
 def notes():
     nt = int(input("Ingrese cuantas notas tiene esta materia: "))
     for i in range(nt):
         sb = (f"Ingrese de que es la nota numero {i+1}: ")
-        
+
 
 
 
