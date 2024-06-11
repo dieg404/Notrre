82537417863d855457fbc8b4b2b54fb8d78b93e4
dieg404 <158373396+dieg404@users.noreply.github.com>
2024-06-10 22:19:40 -0500

    v1

---
+++ codigo.py
@@ -0,0 +1,14 @@
+def notes():
+    nt = int(input("Ingrese cuantas notas tiene esta materia: "))
+    for i in range(nt):
+        sb = (f"Ingrese de que es la nota numero {i+1}: ")
+        
+
+
+
+subject = int(input("Ingrese cuantas materias esta viendo: "))
+subjects = []
+for i in range (subject):
+    sub = input(f"Ingrese su materia numero {i+1}: ")
+    subjects.append(sub)
+
